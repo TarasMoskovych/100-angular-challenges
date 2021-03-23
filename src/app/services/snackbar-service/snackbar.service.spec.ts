7e7a5689032ef65c0352470e678989bcf21a807f
Taras_Moskovych <taras_moskovych@epam.com>
2021-03-23 20:44:22 +0200

    test

--- src/app/services/snackbar-service/snackbar.service.spec.ts
+++ src/app/services/snackbar-service/snackbar.service.spec.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT } from '@angular/common';
-import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
 import { SnackbarComponent } from 'src/app/shared/components';
@@ -22,32 +22,7 @@ describe('SnackbarService', () => {
     service = TestBed.inject(SnackbarService);
   });
 
-  const getSnackbarEl = () => ngDocument.body.querySelector('app-snackbar');
-
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-
-  it('should create container and dynamically append Snackbar component', () => {
-    service.show();
-    expect(getSnackbarEl()).toBeTruthy();
-  });
-
-  it('should have single wrapper container', () => {
-    service.show();
-    service.show();
-    service.show();
-
-    expect(ngDocument.body.querySelectorAll(`.${service['wrapperSelector']}`).length).toBe(1);
-  });
-
-  it('should destroy the component on "afterClosed"', fakeAsync(() => {
-    const instance = service.show();
-
-    jest.spyOn(service['appRef'], 'detachView');
-    instance.afterClosed.emit();
-    tick(500);
-
-    expect(service['appRef'].detachView).toHaveBeenCalled();
-  }));
 });
