8b33a34ee8311824014c3242d218eef71a6e7323
Taras_Moskovych <taras_moskovych@epam.com>
2021-03-23 13:46:46 +0200

    add karma-parallel

--- karma.conf.js
+++ karma.conf.js
@@ -4,14 +4,19 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['parallel', 'jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-parallel'),
     ],
+    parallelOptions: {
+      executors: (Math.ceil(require('os').cpus().length / 2)),
+      shardStrategy: 'round-robin',
+    },
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
