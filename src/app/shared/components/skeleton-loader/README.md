c39a2a8dfe43b123a37a5bd9d270dda47bfa4ea2
Taras_Moskovych <taras_moskovych@epam.com>
2021-03-25 16:10:32 +0200

    create skeleton-loader component

---
+++ src/app/shared/components/skeleton-loader/README.md
@@ -0,0 +1,9 @@
+# Skeleton Loader Component
+
+Build a skeleton loader that takes in a width and height. It should have a nice loading animation as well.
+
+## Tips
+---
+- @Component()
+- @Input()
+- ngStyle
---
+++ src/app/shared/components/skeleton-loader/skeleton-loader.component.ts
@@ -0,0 +1,16 @@
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-loader',
+  templateUrl: './skeleton-loader.component.html',
+  styleUrls: ['./skeleton-loader.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SkeletonLoaderComponent {
+  @Input() width = '100%';
+  @Input() height = '100%';
+
+  get dimensions(): Object {
+    return { width: this.width, height: this.height };
+  }
+}
